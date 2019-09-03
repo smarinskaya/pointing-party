c524ff2af8b9cb79b0a2193f5c515b94406efa67
smarinskaya <svetlana.marinskaya@gmail.com>
2019-09-03 17:15:07 -0400

    Start of tests

--- test/pointing_party/vote_calculator_test.exs
+++ test/pointing_party/vote_calculator_test.exs
@@ -16,7 +16,13 @@ defmodule PointingParty.VoteCalculatorTest do
       #
       # Fix the generator below. It should return a data structure like the one above.
       # Hint: use fixed_map/1, member_of/1, list_of/2, string/2, and nonempty/1.
-      user_generator = constant([])
+      points_map = fixed_map(%{points: integer(1..5)})
+      _ = IO.inspect(points_map)
+      list_of_maps = list_of(points_map, length: 1)
+      _ = IO.inspect(list_of_maps)
+      map_of_list = fixed_map(%{metas: list_of_maps})
+      _ = IO.inspect(map_of_list)
+      user_generator = nonempty(map_of(string(:alphanumeric),map_of_list))
 
       [user_generator: user_generator]
     end
@@ -24,7 +30,7 @@ defmodule PointingParty.VoteCalculatorTest do
     property "calculated vote is a list or an integer", %{user_generator: user_generator} do
       check all users <- user_generator,
                 _ = IO.inspect(users),
-                {_event, winner} = VoteCalculator.calculate_votes(users),
+               # {_event, winner} = VoteCalculator.calculate_votes(users),
                 max_runs: 20 do
         # We'll assert something here
       end
